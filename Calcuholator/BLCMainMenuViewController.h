96b69f35ae5215ac7efce659561004dc2c63da2a
Brandon Wade <brandon.wade3@gmail.com>
2015-06-27 19:12:09 -0500

    Keep having issues with this git business. I had to redo a lot of the work. BLCWhiskeyViewController having issues.

---
+++ Calcuholator/BLCMainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Calcuholator
+//
+//  Created by Brandon Wade on 6/27/15.
+//  Copyright (c) 2015 Brandon Wade. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
